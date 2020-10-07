34a2a33313050a8ab8669bad085c3ba849f6f4a9
Andrew <andrew@glasstube.co.uk>
2020-10-07 19:59:25 +0100

    Add new classes for edditional fields and non cash data

--- ViewerApiClientSDK/CashCountClasses.cs
+++ ViewerApiClientSDK/CashCountClasses.cs
@@ -23,18 +23,35 @@ namespace Tellermate.ViewerApiClientSDK
         public int decimalplaces { get; set; }
         public string htmlsymbol { get; set; }
         public List<Denomination> Denominations { get; set; }
+        public List<NonCash> NonCash { get; set; }
 
     }
+
+
+    public partial class NonCash
+    {
+        public string Format { get; set; }
+        public decimal FaceValue { get; set; }
+        public int decimalplaces { get; set; }
+        public string Name { get; set; }
+    }
+
 
     public partial class CashCount
     {
         public string UnitSerial { get; set; }
         public DateTime CountTime { get; set; }
         public DateTime UploadTime { get; set; }
+
         public int sequencenumber { get; set; }
         public int CountType { get; set; }
         public int Total { get; set; }
+        public int Till { get; set; }
+        public string Operator { get; set;}
+        public string Cashier { get; set; }
+        public string Shift { get; set; }
         public List<Currency> Currencies { get; set; }
+        
 
     }
 
